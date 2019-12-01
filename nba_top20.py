c8425b161e167d6fdb58d5d4005d8f1043b975c7
marom317 <marom317@gmail.com>
2019-12-01 16:11:11 +0200

    add a bar chart race compare players by age

--- nba_top20.py
+++ nba_top20.py
@@ -19,15 +19,21 @@ df_new = df[['Player','Year','PTS','Age','Total_PTS']]
 # find who are the top 20 points scoring
 
 top20players = df_new.groupby(['Player']).sum().sort_values('PTS',ascending=False).head(20)
+
 # reverse the list
+
 top20players = top20players.sort_values('PTS', ascending=True)
+
 # turn the table into list with the name of the players
 
 name_20_lst = list(top20players.index)
 
 top_20_PTS =list(top20players.PTS)
+
+# making a list of strings for the graph
+
 top_20_str = [str(item) for item in top_20_PTS]
-print(top_20_str)
+
 # filter the player column to the 20 top scoring
 
 table_20 = df_new[df_new.Player.isin(name_20_lst)]
@@ -37,7 +43,9 @@ table_20 = df_new[df_new.Player.isin(name_20_lst)]
 table_20.reset_index(drop=True , inplace = True)
 
 print(table_20)
+
 # make the graph
+
 plt.figure(figsize=(15,8))
 y_pos = np.arange(len(name_20_lst))
 plt.xlabel('Total points',  fontdict = {'fontname': 'Comic Sans MS', 'fontsize' : 18})
